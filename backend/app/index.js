57b74dbd57a22acb03345d0d36f7b11299c31df2
MaalyALkaraki <maalykaraki@yahoo.com>
2019-07-03 18:53:22 +0100

    create router for generation and for dragons

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,57 +1,17 @@
-const express =require('express')
+const express =require('express');
+const dragonRouter =require ('./api/dragon');
+const generationRouter =require ('./api/generation');
 const app=express();
 
 const GenerationEngine =require('./generation/engine');
 const engine=new GenerationEngine();
+
+
+app.locals.engine=engine; //since we cant make a cyrcular call from this file to the dragon .js router we can use app.locals to make the engin accessible 
+app.use('/dragon',dragonRouter);
+app.use('/generation',generationRouter);
  engine.start();
-// setTimeout(()=>{
-//     engine.stop();
-// },20000);
-app.get('/dragon/new',(req,res)=>{
-res.json({
-    dragon:engine.generation.newDragon()
-})
-});
+
 
 module.exports=app;
 
-// const Generation =require('./genaration')
-// const generation = new Generation ;
-// console.log ('generation' ,generation);
-
-// const goopy =generation.newDragon();
-// console.log('goopy',goopy);
-
-// setTimeout(()=>{
-//     const mimar =generation.newDragon();
-//     console.log('mimar',mimar);   
-// },15000);
-
-
-
-
-
-// const Dragon=require('./dragon')
-// // console.log("hello Node.js");
-
-// const fooey = new Dragon(
-//     {
-//         birthdate:new Date(),
-//     nickname:'feeoy'
-//     })
-// const baloo=new Dragon(
-//     {
-//     birthdate:new Date(),
-//     nickname:'baloo'
-//     }
-//     );
-// const mimar= new Dragon ();
-// setTimeout(()=>{
-// const gooby = new Dragon ();
-// console.log('gooby' ,gooby);
-// },3000);
-
-// console.log('fooey' ,fooey);
-
-// console.log('baloo' ,baloo);
-// console.log('mimar' ,mimar);
