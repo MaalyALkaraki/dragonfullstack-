54ec47b889b1876aa8c64545b0d96e912a39a80c
MaalyALkaraki <maalykaraki@yahoo.com>
2019-07-03 14:55:03 +0100

    create GenerationEngine

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,53 @@
-const Dragon=require('./dragon')
-// console.log("hello Node.js");
 
-const fooey = new Dragon(
-    {
-        birthdate:new Date(),
-    nickname:'feeoy'
-    })
-const baloo=new Dragon(
-    {
-    birthdate:new Date(),
-    nickname:'baloo'
-    }
-    );
-const mimar= new Dragon ();
+const GenerationEngine =require('./engine');
+const engine=new GenerationEngine();
+engine.start();
 setTimeout(()=>{
-const gooby = new Dragon ();
-console.log('gooby' ,gooby);
-},3000);
+    engine.stop();
+},20000);
 
-console.log('fooey' ,fooey);
 
-console.log('baloo' ,baloo);
-console.log('mimar' ,mimar);
+
+
+
+
+// const Generation =require('./genaration')
+// const generation = new Generation ;
+// console.log ('generation' ,generation);
+
+// const goopy =generation.newDragon();
+// console.log('goopy',goopy);
+
+// setTimeout(()=>{
+//     const mimar =generation.newDragon();
+//     console.log('mimar',mimar);   
+// },15000);
+
+
+
+
+
+// const Dragon=require('./dragon')
+// // console.log("hello Node.js");
+
+// const fooey = new Dragon(
+//     {
+//         birthdate:new Date(),
+//     nickname:'feeoy'
+//     })
+// const baloo=new Dragon(
+//     {
+//     birthdate:new Date(),
+//     nickname:'baloo'
+//     }
+//     );
+// const mimar= new Dragon ();
+// setTimeout(()=>{
+// const gooby = new Dragon ();
+// console.log('gooby' ,gooby);
+// },3000);
+
+// console.log('fooey' ,fooey);
+
+// console.log('baloo' ,baloo);
+// console.log('mimar' ,mimar);
